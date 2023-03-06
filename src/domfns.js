55f64b1a2e2debcd67665a878c9bf905fc9ce2eb
Vitalii-Harkavenko <127046787+Vitalii-Harkavenko@users.noreply.github.com>
2023-03-06 12:52:59 +0100

    Fixed units displaying

--- src/domfns.js
+++ src/domfns.js
@@ -1,16 +1,18 @@
 import getApi from "./apifn";
 
 function renderCurrentWeather(data) {
+    const chosenUnits = document.querySelector('.units-switcher').innerText;
+    
     document.querySelector('.location').innerText = `${data.city.name}, ${data.city.country}`;
-    document.querySelector('.temperature').innerText = `${data.list[0].main.temp}°C`;
+    document.querySelector('.temperature').innerText = `${data.list[0].main.temp}${chosenUnits}`;
     document.querySelector('.description').innerText = data.list[0].weather[0].description;
     document.querySelector('.weather-icon').src = `./images/${data.list[0].weather[0].icon}.png`;
     document.querySelector('.current-humidity').innerText = data.list[0].main.humidity;
     document.querySelector('.current-pressure').innerText = data.list[0].main.pressure;
-    document.querySelector('.current-feels-like').innerText = `${data.list[0].main.feels_like}°C`;
-    document.querySelector('.current-temp-max').innerText = `${data.list[0].main.temp_max}°C`;
-    document.querySelector('.current-temp-min').innerText = `${data.list[0].main.temp_min}°C`;
-    if (document.querySelector('.units-switcher').innerText === '°F') {
+    document.querySelector('.current-feels-like').innerText = `${data.list[0].main.feels_like}${chosenUnits}`;
+    document.querySelector('.current-temp-max').innerText = `${data.list[0].main.temp_max}${chosenUnits}`;
+    document.querySelector('.current-temp-min').innerText = `${data.list[0].main.temp_min}${chosenUnits}`;
+    if (chosenUnits === '°F') {
         document.querySelector('.current-wind-speed').innerText = `${data.list[0].wind.speed}miles/h`;
     } else {
         document.querySelector('.current-wind-speed').innerText = `${data.list[0].wind.speed}meter/sec`;
@@ -18,30 +20,32 @@ function renderCurrentWeather(data) {
 };
 
 function renderForecast(data) {
-    document.querySelector('.forecast-temp-day-one').innerText = `${data.list[8].main.temp}°C`;
+    const chosenUnits = document.querySelector('.units-switcher').innerText;
+    
+    document.querySelector('.forecast-temp-day-one').innerText = `${data.list[8].main.temp}${chosenUnits}`;
     document.querySelector('.forecast-desc-day-one').innerText = data.list[8].weather[0].description;
     document.querySelector('.forecast-icon-day-one').src = `./images/${data.list[8].weather[0].icon}.png`;
-    document.querySelector('.forecast-feels-like-day-one').innerText = `${data.list[8].main.feels_like}°C`;
+    document.querySelector('.forecast-feels-like-day-one').innerText = `${data.list[8].main.feels_like}${chosenUnits}`;
 
-    document.querySelector('.forecast-temp-day-two').innerText = `${data.list[16].main.temp}°C`;
+    document.querySelector('.forecast-temp-day-two').innerText = `${data.list[16].main.temp}${chosenUnits}`;
     document.querySelector('.forecast-desc-day-two').innerText = data.list[16].weather[0].description;
     document.querySelector('.forecast-icon-day-two').src = `./images/${data.list[16].weather[0].icon}.png`;
-    document.querySelector('.forecast-feels-like-day-two').innerText = `${data.list[16].main.feels_like}°C`;
+    document.querySelector('.forecast-feels-like-day-two').innerText = `${data.list[16].main.feels_like}${chosenUnits}`;
 
-    document.querySelector('.forecast-temp-day-three').innerText = `${data.list[24].main.temp}°C`;
+    document.querySelector('.forecast-temp-day-three').innerText = `${data.list[24].main.temp}${chosenUnits}`;
     document.querySelector('.forecast-desc-day-three').innerText = data.list[24].weather[0].description;
     document.querySelector('.forecast-icon-day-three').src = `./images/${data.list[24].weather[0].icon}.png`;
-    document.querySelector('.forecast-feels-like-day-three').innerText = `${data.list[24].main.feels_like}°C`;
+    document.querySelector('.forecast-feels-like-day-three').innerText = `${data.list[24].main.feels_like}${chosenUnits}`;
 
-    document.querySelector('.forecast-temp-day-four').innerText = `${data.list[32].main.temp}°C`;
+    document.querySelector('.forecast-temp-day-four').innerText = `${data.list[32].main.temp}${chosenUnits}`;
     document.querySelector('.forecast-desc-day-four').innerText = data.list[32].weather[0].description;
     document.querySelector('.forecast-icon-day-four').src = `./images/${data.list[32].weather[0].icon}.png`;
-    document.querySelector('.forecast-feels-like-day-four').innerText = `${data.list[32].main.feels_like}°C`;
+    document.querySelector('.forecast-feels-like-day-four').innerText = `${data.list[32].main.feels_like}${chosenUnits}`;
     
-    document.querySelector('.forecast-temp-day-five').innerText = `${data.list[39].main.temp}°C`;
+    document.querySelector('.forecast-temp-day-five').innerText = `${data.list[39].main.temp}${chosenUnits}`;
     document.querySelector('.forecast-desc-day-five').innerText = data.list[39].weather[0].description;
     document.querySelector('.forecast-icon-day-five').src = `./images/${data.list[39].weather[0].icon}.png`;
-    document.querySelector('.forecast-feels-like-day-five').innerText = `${data.list[39].main.feels_like}°C`;
+    document.querySelector('.forecast-feels-like-day-five').innerText = `${data.list[39].main.feels_like}${chosenUnits}`;
 };
 
 export async function renderWeather(defaultInput = '', updateUnits = false) {
