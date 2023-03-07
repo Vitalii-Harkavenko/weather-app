2b0e5b51aa79f61b8fc53565254ebed98239583d
Vitalii-Harkavenko <127046787+Vitalii-Harkavenko@users.noreply.github.com>
2023-03-07 17:08:40 +0100

    Delete src directory

--- src/apifn.js
+++
@@ -1,6 +0,0 @@
-export default function getApi(location, units) {
-    return fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=632fd6bb413cdf25606b95ce9dcf7790&units=${units}`,
-        {mode: 'cors'}
-    )
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles.css';
-import createPage from './createpage.js';
-import searchWeather, { renderWeather, switchUnits } from './domfns.js';
-
-createPage();
-switchUnits();
-searchWeather();
-renderWeather('London');
